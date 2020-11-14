95a46619439bc22a57d60a8821f3be4db9130f4e
alexggmark <alexgmark@gmail.com>
2020-11-14 18:02:58 +0000

    Setting up a better apollo-server-express implementation

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
-import express from 'express'
-const app = express()
-
-import services from './services'
-
-const serviceNames = Object.keys(services)
-
-for (let i = 0; i < serviceNames.length; i++) {
-	const name = serviceNames[i]
-	if (name === 'graphql') {
-		services[name].applyMiddleware({ app })
-	} else {
-		app.use(`/${name}`, services[name])
-	}
-}
-
-app.listen(8000, () => console.log('Running'))
+import express  from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+ 
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+ 
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+ 
+const server = new ApolloServer({ typeDefs, resolvers });
+ 
+const app = express();
+server.applyMiddleware({ app });
+ 
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
